6b0a47e95f14efe2048abe84e259af7fd1708454
Liam Rand <cobaplatinum@gmail.com>
2024-10-30 05:18:16 -0500

    Started Work on Editor Console

---
+++ Editor/Core/PlatinumConsoleEditorWindow.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace CobaPlatinum.DebugTools.PlatinumConsole
+{
+    public class PlatinumConsoleEditorWindow : EditorWindow
+    {
+        [MenuItem("Coba Platinum/Debug Tools/Platinum Console")]
+        public static void OpenEditorWindow()
+        {
+            PlatinumConsoleEditorWindow window = GetWindow<PlatinumConsoleEditorWindow>();
+            window.titleContent = new UnityEngine.GUIContent("Platinum Console");
+        }
+
+        private void CreateGUI()
+        {
+            VisualElement root = rootVisualElement;
+            var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(
+                "Packages/com.cobaplatinum.devtools/Resources/UI Documents/PlatinumConsoleEditorWindow.uxml");
+
+            VisualElement tree = visualTree.Instantiate();
+            root.Add(tree);
+        }
+    }
+}
